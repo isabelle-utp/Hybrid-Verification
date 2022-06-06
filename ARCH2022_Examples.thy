0bceffbca581c61c64d4d6fcd2eb665aa76ac55b
yonoteam <jonjulian23@gmail.com>
2022-06-06 20:57:00 +0200

    More attempts on dynamics parametric switching

--- ARCH2022_Examples.thy
+++ ARCH2022_Examples.thy
@@ -1060,14 +1060,40 @@ lemma "(w \<ge> 0 \<and> d \<ge> 0 \<and> -2 \<le> a \<and> a \<le> 2 \<and> b\<
     \<sqinter> \<questiondown>True?) 
   INV (w\<^sup>2*x\<^sup>2+y\<^sup>2 \<le> c \<and> d \<ge>0 \<and> w\<ge>0)
   ] (w\<^sup>2*x\<^sup>2+y\<^sup>2 \<le> c)"
+  apply (subst change_loopI[where I="(w\<^sup>2*x\<^sup>2+y\<^sup>2 \<le> c \<and> d \<ge>0 \<and> w\<ge>0 \<and> -2 \<le> a \<and> a \<le> 2 \<and> b\<^sup>2 \<ge> 1/3)\<^sup>e"])
   apply (rule hoare_loopI)
+    apply (simp add: wp)
+    apply (rule diff_cut_on_rule[where C="(0 \<le> d \<and> 0 \<le> w \<and> -2 \<le> a \<and> a \<le> 2 \<and> b\<^sup>2 \<ge> 1/3)\<^sup>e"])
+         apply (rule fbox_inv[where I="(0 \<le> d \<and> 0 \<le> w \<and> -2 \<le> a \<and> a \<le> 2 \<and> b\<^sup>2 \<ge> 1/3)\<^sup>e"])
+           apply (expr_simp add: le_fun_def)
+          apply (simp only: expr_defs hoare_diff_inv_on fbox_diff_inv_on)?
+          apply (intro diff_inv_on_raw_conjI; (diff_inv_on_ineq "(0)\<^sup>e" "(0)\<^sup>e")?)
+     apply (expr_simp add: le_fun_def)
+  apply (subst hoare_conj_posc, intro conjI)
+    apply (simp add: wp hoare_conj_posc, intro conjI)
+     apply (rule diff_weak_on_rule, expr_auto)
+     apply (auto simp: field_simps)[1]
+
+  oops
     apply (simp add: wp hoare_conj_posc, intro conjI)
         apply (rule diff_cut_on_rule[where C="(0 \<le> d \<and> 0 \<le> w)\<^sup>e"])
          apply (rule fbox_inv[where I="(0 \<le> d \<and> 0 \<le> w)\<^sup>e"])
            apply (expr_simp add: le_fun_def)
           apply (simp only: expr_defs hoare_diff_inv_on fbox_diff_inv_on)?
-  apply (rule diff_inv_on_raw_conjI)
-  apply (diff_inv_on_ineq "(0)\<^sup>e" "(0)\<^sup>e")
+          apply (rule diff_inv_on_raw_conjI)
+           apply (diff_inv_on_ineq "(0)\<^sup>e" "(0)\<^sup>e")
+          apply (diff_inv_on_ineq "(0)\<^sup>e" "(0)\<^sup>e")
+         apply (expr_simp add: le_fun_def)
+        apply (rule diff_cut_on_rule[where C="(w\<^sup>2*x\<^sup>2+y\<^sup>2 \<le> c)\<^sup>e"])
+         apply (rule fbox_inv[where I="(w\<^sup>2*x\<^sup>2+y\<^sup>2 \<le> c)\<^sup>e"])
+           apply (expr_simp add: le_fun_def)
+          apply (simp only: expr_defs hoare_diff_inv_on fbox_diff_inv_on)?
+  apply (diff_inv_on_single_ineq_intro "(4*w\<^sup>2*x*y+2*y*(-w\<^sup>2*x-2*d*w*y))\<^sup>e" "(0)\<^sup>e"; expr_simp)
+         apply(auto intro!: poly_derivatives simp: field_simps power2_eq_square)[1]
+  oops
+  apply (diff_inv_on_ineq "(4*w\<^sup>2*x*y+2*y*(-w\<^sup>2*x-2*d*w*y))\<^sup>e" "(0)\<^sup>e")
+
+
          apply (diff_cut_ineq "(0 \<le> d)\<^sup>e" "(0)\<^sup>e" "(0)\<^sup>e")
   apply (rule C="" in diff_cut_rule
   using hoare_posc_distrib_conj[of "(w\<^sup>2 * x\<^sup>2 + y\<^sup>2 \<le> c \<and> 0 \<le> d \<and> 0 \<le> w)\<^sup>e" _ ]
